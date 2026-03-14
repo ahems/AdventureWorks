983a7787036f8ed67d9a12b1ba54435a060973fa
Adam Hems <adamhems@yahoo.com>
2026-03-14 14:22:53 -0500

    various version updates

--- api-mcp/AppHost/Program.cs
+++ api-mcp/AppHost/Program.cs
@@ -1,10 +1,10 @@
-using Aspire.Hosting.Azure.Functions;
+using Aspire.Hosting.Azure;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AdventureWorks>("adventureworks-mcp")
 	   .WithExternalHttpEndpoints();
 
-builder.AddAzureFunctionsProject<Projects.ApiFunctions>("api-functions");
+builder.AddAzureFunctionsProject<Projects.api_functions>("api-functions");
 
 builder.Build().Run();
